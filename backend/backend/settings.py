0d957309aab7c2f8f736bffa4e15ad16bcfb07aa
MihaiO99 <mihaioancea00798@gmail.com>
2024-06-08 00:24:07 +0300

    adjust yml

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -52,8 +52,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django_recaptcha",
     "api",
+    "rest_framework",
     "corsheaders",
 ]
 
